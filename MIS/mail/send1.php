9faa246ba7a3ca653268d3ffe3b50c139e4796c8
Gaurav <gauravvshharma@gmail.com>
2022-03-11 02:10:53 +0530

    DIS initial commit

--- MIS/mail/send1.php
+++ MIS/mail/send1.php
@@ -1,18 +1,26 @@
 <?php
-  $name=$_POST['name'];
+
+
+
+
+
+
+$name=$_POST['name'];
   $mobile=$_POST['mobile'];
   $city=$_POST['city'];
   $cources=$_POST['cources'];
   $subject=$_POST['subject'];
   
   // $message1=$_POST['message'];
-$to = "dimple.lodiaofficial@gmail.com";
-$subject = "MIS Admission Lead";
+  
+
+$to = "Leads@jbcollege.in";
+$subject = "REPEAT INQUIRY ";
 
 $message = "
 <html>
 <head>
-<title>Leads MIS</title>
+<title>Leads</title>
 </head>
 <body>
 <table rules='all' style='border-color:#666;width:100%;font-family:Helvetica,Arial,sans-serif;background-color:#f7f7f7;color:#333;padding:0;margin:0'>
@@ -20,12 +28,16 @@ $message = "
              <td>
              <table style='width:80%;background:#ffffff;margin:15px auto;font-size:12px;line-height:18px' cellpadding='10'>
              <tbody><tr>
-             </tr>
-             <tr><td>Name   : $name</td></tr>
-             <tr><td>Mobile : $mobile</td></tr>	
-             <tr><td>City   : $city</td></tr>	
-             <tr><td>course : $cources</td></tr>
-             <tr><td>Source : $subject</td></tr>
+             </tr><tr><td>Name : $name</td></tr>
+             <tr><td>Mobile: $mobile</td></tr>	
+             <tr><td>City  : $city</td></tr>	
+             <tr><td>course: $cources</td></tr>
+             <tr><td>SourCe: $subject</td></tr>
+             
+             
+
+
+            
              <tr>
              <td style='padding:0 5px'>
              <table style='font-size:12px;line-height:18px;margin:0;padding:0'>
@@ -42,15 +54,34 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: <leads@jbcollege.in>' . "\r\n";
+$headers .= 'From: <Leads@jbcollege.in>' . "\r\n";
 
 $ok=mail($to,$subject,$message,$headers);
 
 if ($ok){
-  echo "Thanks! Your form has been sent successfully.";
+      
+ //die();
+
+   //echo '1';
+   //exit;
+  header("Location: https://www.jbcollege.in/apply/thank-you.html");
   die();
 }else{
-   echo "Your form is not submitted Call 9811999868 Now";
+   
+    echo "Your form is not submitted call 9999888321 now";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
